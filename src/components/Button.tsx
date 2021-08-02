c993c06152d43f9085a3d7c107e89ffd5e722520
MichaelHenrique <michaelhenriqued@gmail.com>
2021-08-02 18:47:12 -0300

    Adding Home page and styling with Sass

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
+import { ButtonHTMLAttributes } from "react";
 
-export function Button(){
-    const [counter, setCounter] = useState(0);
+import '../styles/button.scss'
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-    function increment() {
-      setCounter(counter + 1);
-    }
-
+export function Button(props: ButtonProps){
   return(
-    <button onClick={increment}>
-      {counter}
-    </button>
+    <button className="button" {...props} />
   )
 }
