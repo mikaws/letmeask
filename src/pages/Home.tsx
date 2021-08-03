28db1ecacd7a716456dd2e95139b6382d7ff5522
MichaelHenrique <michaelhenriqued@gmail.com>
2021-08-02 22:39:20 -0300

    Google Authentication with Firebase using useContext and useAuth(custom hook)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,21 @@
 import { useHistory } from 'react-router-dom';
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
-import "../styles/auth.scss"
-
-import { Button } from '../components/Button';
+import "../styles/auth.scss";
 
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function handleCreateRoom() {
-    
+  async function handleCreateRoom() {
+    if(!user){
+      await signInWithGoogle();
+    }
     history.push('/rooms/new');
   }
 
